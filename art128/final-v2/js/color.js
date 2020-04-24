f69a5f7f81e37006f5449f08c1ac0ffbf28219be
Mark Turina <turina@hawaii.edu>
2020-04-24 12:25:52 -1000

    updated js for header color

--- art128/final-v2/js/color.js
+++ art128/final-v2/js/color.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     jQuery('.site-header.sticky').css("background-color", "transparent");
     jQuery(window).scroll(function() {
         if (jQuery(window).scrollTop() > 0) {
-            jQuery('.site-header.sticky').css("background-color", "blue");              
+            jQuery('.site-header.sticky').css("background-color", "white");              
         } else {
             jQuery('.site-header.sticky').css("background-color", "transparent");
         }
